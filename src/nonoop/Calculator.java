df8e047c338dc4f1a0ce912a52e49c2dfadce657
CGM <mrmouse.guru@gmail.com>
2024-05-20 15:16:49 +0700

    added structured calculating a * b

---
+++ src/nonoop/Calculator.java
@@ -0,0 +1,14 @@
+package nonoop;
+
+public class Calculator {
+
+    public static void main(String[] args) {//start
+        
+        int a = 2;
+        int b = 2;
+        int c = a * b;
+        System.out.println("ket qua la: " + c);
+        
+        return;}//end
+
+}

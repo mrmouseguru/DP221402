5451ac286fb0193d7c722b33fa181388bf355268
CGM <mrmouse.guru@gmail.com>
2024-06-03 13:50:53 +0700

    initalize swing calculator app

---
+++ src/buoi3/swing/CalculatorWindow.java
@@ -0,0 +1,21 @@
+package buoi3.swing;
+
+import javax.swing.JFrame;
+
+public class CalculatorWindow extends JFrame{
+
+    //field
+    private String title;
+
+
+    //function , method
+    CalculatorWindow(){
+        title = "FrameViewer";
+        setSize(400, 400);
+        setVisible(true);
+
+    }
+
+
+
+}

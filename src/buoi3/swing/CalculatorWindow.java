68efbba8ad826006d19d10268af0364eb0cdaec6
CGM <mrmouse.guru@gmail.com>
2024-06-03 14:11:37 +0700

    update controls to JPanel, JFrame

--- src/buoi3/swing/CalculatorWindow.java
+++ src/buoi3/swing/CalculatorWindow.java
@@ -1,20 +1,46 @@
 package buoi3.swing;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CalculatorWindow extends JFrame{
 
     //field
     private String title;
+    private JPanel jPanelRemote;
+    private JLabel jLabelInput1Remote, jLabelInput2Remote, 
+    jLabelOutputRemote;
+    private JTextField jTextFieldInput1Remote, jTextFieldInput2Remote;
+    private JButton addButtonRemote, subButtonRemote, mulButtonRemote, divButtonRemote;
 
 
     //function , method
     CalculatorWindow(){
-        title = "FrameViewer";
+        buildPanel();
+        add(jPanelRemote);
+        title = "Frame Viewer";
+        setTitle(title);
         setSize(400, 400);
         setVisible(true);
 
     }
+
+    public void buildPanel() {
+        jPanelRemote = new JPanel();
+        jLabelInput1Remote = new JLabel("input1");
+        jPanelRemote.add(jLabelInput1Remote);
+        jTextFieldInput1Remote = new JTextField(10);
+        jPanelRemote.add(jTextFieldInput1Remote);
+        //jPanelRemote.setBackground(Color.BLUE);
+        
+    }
+
+
 
 
 

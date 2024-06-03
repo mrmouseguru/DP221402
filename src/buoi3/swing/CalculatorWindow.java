62a78a57f00bc7da836e116084e537f8150b8006
CGM <mrmouse.guru@gmail.com>
2024-06-03 14:43:40 +0700

    updated adding to calculator

--- src/buoi3/swing/CalculatorWindow.java
+++ src/buoi3/swing/CalculatorWindow.java
@@ -1,6 +1,8 @@
 package buoi3.swing;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,7 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class CalculatorWindow extends JFrame{
+public class CalculatorWindow extends JFrame implements ActionListener{
 
     //field
     private String title;
@@ -36,8 +38,27 @@ public class CalculatorWindow extends JFrame{
         jPanelRemote.add(jLabelInput1Remote);
         jTextFieldInput1Remote = new JTextField(10);
         jPanelRemote.add(jTextFieldInput1Remote);
+        jLabelInput2Remote = new JLabel("input2");
+        jTextFieldInput2Remote = new JTextField(10);
+        jLabelOutputRemote = new JLabel("Output");
+        jPanelRemote.add(jLabelInput2Remote);
+        jPanelRemote.add(jTextFieldInput2Remote);
+        jPanelRemote.add(jLabelOutputRemote);
+        addButtonRemote = new JButton("ADD");
+        addButtonRemote.addActionListener(this);//Remote của CalculatorWindow
+        jPanelRemote.add(addButtonRemote);
+        
         //jPanelRemote.setBackground(Color.BLUE);
         
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        double num1 = Double.parseDouble(jTextFieldInput1Remote.getText());
+        double num2 = Double.parseDouble(jTextFieldInput2Remote.getText());
+        double result = num1 + num2;
+
+        jLabelOutputRemote.setText("" + result);
     }
 
 
